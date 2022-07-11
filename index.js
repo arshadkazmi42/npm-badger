c0ab54f519306c11873304b9fdf227dad2a87247
Arshad Kazmi <arshadkazmi42@gmail.com>
2022-07-11 18:58:28 +0530

    Tests added. Move badger logic to separte file

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
 #!/usr/bin/env node
 
-const Fs = require('fs');
-const npmBadge = require('npm-badge-generator');
-const { UserRepo } = require('@gh-conf/gh-conf-parse');
+const badger = require('./npm-badger');
 
-
-const generateBadge = () => {
-
-  const args = process.argv;
-  const path = (args.length > 2) ? args[2] : process.cwd();
-
-  const { repository, username } = UserRepo();
-  const badges = npmBadge.generate(repository, repository, username);
-
-  const readme = Fs.readFileSync(`${path}/README.md`, 'utf-8');
-  const readmeLines = readme.split('\n');
-
-  readmeLines[1] = `\n${badges}\n${readmeLines[1]}`;
-
-  const readmeWithBadges = readmeLines.join('\n');
-
-  Fs.writeFileSync(`${path}/README.md`, readmeWithBadges, 'utf-8');
-};
-
-generateBadge();
+badger.generate();
--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,37 @@
-// const { expect } = require('chai');
-const { exec } = require('child_process');
+const Fs = require('fs');
+const {
+  expect
+} = require('chai');
+
+const badger = require('../npm-badger');
+
+const BADGES = [
+  '[![Build](https://github.com/arshadkazmi42/npm-badger/actions/workflows/nodejs.yml/badge.svg)](https://github.com/arshadkazmi42/npm-badger/actions/workflows/nodejs.yml)',
+  '[![NPM Version](https://img.shields.io/npm/v/npm-badger.svg)](https://www.npmjs.com/package/npm-badger)',
+  '[![NPM Downloads](https://img.shields.io/npm/dt/npm-badger.svg)](https://www.npmjs.com/package/npm-badger)',
+  '[![Github Repo Size](https://img.shields.io/github/repo-size/arshadkazmi42/npm-badger.svg)](https://github.com/arshadkazmi42/npm-badger)',
+  '[![LICENSE](https://img.shields.io/npm/l/npm-badger.svg)](https://github.com/arshadkazmi42/npm-badger/blob/master/LICENSE)',
+  '[![Contributors](https://img.shields.io/github/contributors/arshadkazmi42/npm-badger.svg)](https://github.com/arshadkazmi42/npm-badger/graphs/contributors)',
+  '[![Commit](https://img.shields.io/github/last-commit/arshadkazmi42/npm-badger.svg)](https://github.com/arshadkazmi42/npm-badger/commits/master)'
+];
 
 
 describe('Badger updates readme', () => {
+  before(() => {
+    let readmeFile = Fs.readFileSync(`${process.cwd()}/README.md`, 'utf-8');
+    for (const badge of BADGES) {
+      readmeFile = readmeFile.replace(badge, '');
+    }
+
+    Fs.writeFileSync(`${process.cwd()}/README.md`, readmeFile, 'utf-8');
+  });
   it('Should add badges to the README.md file', () => {
-    exec('../index arshad/test');
+    
+    badger.generate(process.cwd());
+
+    const readmeFile = Fs.readFileSync(`${process.cwd()}/README.md`, 'utf-8');
+    for (const badge of BADGES) {
+      expect(readmeFile).to.include(badge);
+    }
   });
 });
