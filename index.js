f2d6451a200bdcc66856067a60fcb5b998bb0d6a
Arshad Kazmi <arshadkazmi42@gmail.com>
2022-07-11 15:24:40 +0530

    Add logic to generate and update readme

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
 #!/usr/bin/env node
 
 
+const { UserRepo } = require('@gh-conf/gh-conf-parse');
+const npmBadge = require('npm-badge-generator');
 const logSymbols = require('log-symbols');
+const ora = require('ora');
+
+
+const MESSAGES = {
+  GENERATING: 'Generating Badges',
+  WRITING: 'Adding badges to README.md',
+  COMPLETE: 'Badges added successfully'
+};
+
+
+const generateBadge = (path) => {
+
+  const spinner = ora(MESSAGES.GENERATING).start();
+
+  const { repository, username } = UserRepo();
+  const badges = npmBadge.generate(repository, repository, username);
+
+  spinner.text = MESSAGES.WRITING;
+
+  const readme = Fs.readSync(`${path}/README.md`, 'utf-8');
+  const readmeLines = readme.split('\n');
+
+  readmeLines[1] = `\n${npmBadge}\n${readmeLines[1]}`;
+
+  const readmeWithBadges = readmeLines.join('\n');
+
+  Fs.writeSync(`${path}/README.md`, readmeWithBadges, 'utf-8');
+
+  spinner.stop;
+  console.log(logSymbols.success, MESSAGES.COMPLETE);
+}
