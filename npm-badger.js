b82bc2648b2bd6ed75f85b8928a5042fa8255051
tarannum-2002 <tarannum.cs20@bmsce.ac.in>
2022-12-08 19:51:14 +0530

    modified to resolve issue #2

--- npm-badger.js
+++ npm-badger.js
@@ -1,15 +1,30 @@
 const Fs = require('fs');
 const npmBadge = require('npm-badge-generator');
 const { UserRepo } = require('@gh-conf/gh-conf-parse');
+const fetch = require('node-fetch');
+const _ = require('lodash');
+const Strmat = require('strmat');
 
 
 const externals = {};
 
 
 externals.generate = (path) => {
+  
+  
+  const response = fetch(Strmat.format(
+    UserRepo['REPO_DETAILS'], {
+      username: username,
+      repository: repository
+    }
+  ));
 
+  const repositoryData = response.json();
+  const owner = _.get(repositoryData, 'parent.owner.login');
+  const ownerRepo = _.get(repositoryData, 'parent.name');
+  
   const { repository, username } = UserRepo();
-  const badges = npmBadge.generate(repository, repository, username);
+  const badges = npmBadge.generate(repository, repository, ownerRepo);
 
   const readme = Fs.readFileSync(`${path}/README.md`, 'utf-8');
   const readmeLines = readme.split('\n');
